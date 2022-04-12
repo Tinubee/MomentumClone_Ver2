7215da7da6ad2f6e49cdedc03b1683e112e6fe08
KIMHYOUNGMIN <rlagud2005@naver.com>
2022-04-12 14:10:09 +0900

    Update background.js

--- js/background.js
+++ js/background.js
@@ -5,3 +5,4 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 const bgImage = document.createElement("img");
 bgImage.src = `img/${chosenImage}`;
 document.body.appendChild(bgImage);
+document.body.style.backgroundImage = `linear-gradient(rgba(0, 0, 0, 0.4),rgba(0, 0, 0, 0.4)), ${bgImage.src}`;
